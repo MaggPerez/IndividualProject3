974ef871584c18d451c1a55c8532f8fefde0882d
MaggPerez <perezmag5137@gmail.com>
2025-11-26 23:15:46 -0500

    created main screen test

--- app/src/main/java/com/example/individualproject3/MainScreen.kt
+++ app/src/main/java/com/example/individualproject3/MainScreen.kt
@@ -1,10 +1,23 @@
 package com.example.individualproject3
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
@@ -15,10 +28,41 @@ import com.example.individualproject3.viewmodels.LoginRegistrationViewModel
 fun MainScreen(
     navController: NavHostController,
     modifier: Modifier = Modifier,
-//    viewModel: LoginRegistrationViewModel = viewModel()
 ) {
-    Column() {
-        Text(text = "Hello")
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxSize()
+    ) {
+        Box(
+            modifier = Modifier.background(Color.Red)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.robot),
+                contentDescription = "Logo"
+            )
+        }
+
+        Text(
+            text = "PuzzleBot",
+            style = MaterialTheme.typography.headlineLarge
+        )
+
+        OutlinedButton(
+            onClick = { navController.navigate("login_screen") }
+        ) {
+            Text(
+                text = "Login"
+            )
+        }
+
+        OutlinedButton(
+            onClick = { navController.navigate("register_screen") }
+        ) {
+            Text(
+                text = "Register"
+            )
+        }
     }
 }
 
